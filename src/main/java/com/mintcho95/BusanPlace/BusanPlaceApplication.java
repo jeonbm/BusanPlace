a5cb5b9295d91d2c56cc320db0eb04bdf491913d
mint <return-1@naver.com>
2023-12-26 01:20:41 +0900

    프로젝트 생성

---
+++ src/main/java/com/mintcho95/BusanPlace/BusanPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.mintcho95.BusanPlace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusanPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusanPlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mintcho95/BusanPlace/BusanPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mintcho95.BusanPlace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusanPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
